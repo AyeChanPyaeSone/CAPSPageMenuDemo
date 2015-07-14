f7f366e03d7884be61dc7e92f81d858e58a0d9ca
AyeChanPyaeSone <ayechanpyaesone.ac@gmail.com>
2015-07-14 14:48:35 +0800

    Initial Commit

---
+++ CAPSPageMenuDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CAPSPageMenuDemo
+//
+//  Created by AyeChan PyaeSone on 14/7/15.
+//  Copyright (c) 2015 Tware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CAPSPageMenuDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CAPSPageMenuDemo
+//
+//  Created by AyeChan PyaeSone on 14/7/15.
+//  Copyright (c) 2015 Tware. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
